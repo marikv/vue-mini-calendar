c9b36e5754b9bc5cf554a1f50b99cdc073825b22
MARIK-PC\marik <marin.vartan@gmail.com>
2018-05-03 22:55:42 +0300

    new version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-var vueMiniCalendar = require('./vueMiniCalendar')
+//var vueMiniCalendar = require('./vueMiniCalendar')
+//module.exports = vueMiniCalendar
 
-module.exports = vueMiniCalendar
+import vueMiniCalendar from './vueMiniCalendar';
+
+export default {
+    install (Vue) {
+        Vue.component('vueMiniCalendar', vueMiniCalendar);
+    }
+}
+
+export { vueMiniCalendar }
